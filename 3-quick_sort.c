7201d217b668dea6b8b589cde2fcef43e08b3836
walid salmi <salamiwalid1999@gmail.com>
2023-09-29 21:36:06 +0000

    tasks

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,32 +1,30 @@
 #include "sort.h"
 
 /**
- * partition - Rearranges a partition of an array of integers for sorting.
- * @array: Array of integers to be partitioned.
- * @low: Index in array that begins the partition.
- * @high: Index in array that ends the partition.
- * @size: Number of elements in the array.
- *
- *
- * Return: New index at which to start the next recursive partition.
+ * partition - scans a partition of an array of integers for values less than
+ * pivot value, and swaps them with first value in partition, then swaps pivot
+ * value with first value in partition
+ * @array: array of integers to be sorted
+ * @low: index in array that begins partition
+ * @high: index in array that ends partition
+ * @size: amount of elements in array
+ * Return: new index at which to start new recursive partition
  */
 int partition(int *array, int low, int high, size_t size)
 {
-int i, j,
-pivot,
-temp;
+	int i, j, pivot, temp;
 
 	pivot = array[high];
 	i = low;
 	for (j = low; j < high; j++)
 	{
-			if (array[j] < pivot)
+		if (array[j] < pivot)
 		{
 			temp = array[i];
 			array[i] = array[j];
 			array[j] = temp;
 			if (array[i] != array[j])
-					print_array(array, size);
+				print_array(array, size);
 			i++;
 		}
 	}
@@ -34,9 +32,10 @@ temp;
 	array[i] = array[high];
 	array[high] = temp;
 	if (array[i] != array[high])
-			print_array(array, size);
+		print_array(array, size);
 	return (i);
 }
+
 /**
  * quicksort - recursively sorts array of integers by separating into two
  * partitions, using Lomuto quick sort
